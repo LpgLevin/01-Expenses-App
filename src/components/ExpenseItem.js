35b11c51004360b1a9bc9c35658234d072346b5e
LpgLevin <lpglevin@gmail.com>
2023-06-22 16:15:28 +0100

    ExpensesItem

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,23 @@
+import './ExpenseItem.css';
+
+function ExpenseItem(expenseProps) {
+
+    const expenseDate = new Date(2021, 2, 28);
+    const expesnseTitle = 'Car Insurance';
+    const expenseAmount = 294.67;
+
+    return (
+
+        <div className="expense-item">
+            <div>{ expenseDate.toString()}</div>
+            <div className="expense-item__description">
+                <h2>{ expesnseTitle }</h2>
+            </div>
+            <div className="expense-item__price">£{ expenseAmount }</div>
+        </div>
+
+    );
+
+};
+
+export default ExpenseItem;
