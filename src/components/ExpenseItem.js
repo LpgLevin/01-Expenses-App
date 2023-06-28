3a390fc1db997e482a8f2f81f40453816edf3307
LpgLevin <lpglevin@gmail.com>
2023-06-28 09:58:42 +0100

    format dates

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,13 +2,23 @@ import './ExpenseItem.css';
 
 function ExpenseItem({ date, title, amount }) {
 
+    const day = date.toLocaleString( "en-UK", { day: "2-digit" } );
+    const month = date.toLocaleString( "en-UK", { month: "long" } );
+    const year = date.getFullYear();
+
     return (
 
         <div className="expense-item">
-            <div>{ date.toISOString() }</div>
+            <div>
+                <div>{ day }</div>
+                <div>{ month }</div>
+                <div>{ year }</div> 
+            </div>
+
             <div className="expense-item__description">
                 <h2>{ title }</h2>
             </div>
+            
             <div className="expense-item__price">£{ amount }</div>
         </div>
 
