d3205f6fb42fec281d35fc029b82ca847c62a611
LpgLevin <lpglevin@gmail.com>
2023-07-09 20:24:48 +0100

    handleSubmit

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -44,9 +44,23 @@ function ExpenseForm() {
         setEnteredDate( event.target.value );
     };
 
+    function submitHandler( event ) {
+
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+
+        console.log(expenseData)
+
+    };
+
     return (
 
-        <form>
+        <form onSubmit={ submitHandler } >
             <div className="new-expense__controls" >
 
                 <div className="new-expense__control" >
