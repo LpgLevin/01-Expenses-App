793de66b16dd3f5895c9c307ad0f6be65c8aba6e
LpgLevin <lpglevin@gmail.com>
2023-07-19 15:18:43 +0100

    if statement vs ternary vs && for conditional rendering in Expense.js

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,45 +7,64 @@ import ExpensesFilter from "../Filter/ExpensesFilter";
 function Expenses( { items } ) {
 
     const [ filteredYear, setFilteredYear ] = useState( "2020" );
-    const [ filteredList, setFilteredList ] = useState( items );
+    // const [ filteredList, setFilteredList ] = useState( items );
 
     function filterChangeHandler( selectedYear ) {
 
         setFilteredYear( selectedYear );  
 
-        let filteredExpenses = items;
-
-        if ( selectedYear === "2019" ) {
-                 filteredExpenses = items.filter( function( expense ) {
-                return expense.date.getFullYear() === 2019;
-              } );
+        // if ( selectedYear === "2019" ) {
+            //      filteredExpenses = items.filter( function( expense ) {
+            //     return expense.date.getFullYear() === 2019;
+            //   } );
              
-        }; 
+        // }; 
 
 
-        if ( selectedYear === "2020" ) {
-                 filteredExpenses = items.filter( function( expense ) {
-                return expense.date.getFullYear() === 2020;
-              } );
-        };
+        // if ( selectedYear === "2020" ) {
+        //          filteredExpenses = items.filter( function( expense ) {
+        //         return expense.date.getFullYear() === 2020;
+        //       } );
+        // };
 
 
-        if ( selectedYear === "2021" ) {
-                 filteredExpenses = items.filter( function( expense ) {
-                return expense.date.getFullYear() === 2021;
-              } );
-        };
+        // if ( selectedYear === "2021" ) {
+        //          filteredExpenses = items.filter( function( expense ) {
+        //         return expense.date.getFullYear() === 2021;
+        //       } );
+        // };
 
          
-        if ( selectedYear === "2022" ) {
-                 filteredExpenses = items.filter( function( expense ) {
-                return expense.date.getFullYear() === 2022;
-              } );
+        // if ( selectedYear === "2022" ) {
+        //          filteredExpenses = items.filter( function( expense ) {
+        //         return expense.date.getFullYear() === 2022;
+        //       } );
               
-        };        
+        // };        
         
-        setFilteredList( filteredExpenses );
+        // setFilteredList( filteredExpenses );
     };
+
+        const filteredList = items.filter( function( expense ) {
+            return expense.date.getFullYear().toString() === filteredYear;
+        });
+
+                
+        let expensesContent = <p>No expenses found.</p>;
+
+        if ( filteredList.length > 0 ) {
+
+        expensesContent = filteredList.map( ( expense )=> (
+                        
+            <ExpenseItem 
+                key={ expense.id }
+                title={ expense.title } 
+                amount={ expense.amount } 
+                date={ expense.date } 
+            /> 
+          ) ); 
+                        
+        };
 
     return (
 
@@ -56,13 +75,41 @@ function Expenses( { items } ) {
                     onChangeFilter={ filterChangeHandler } 
                 />    
                 
-                { filteredList.map( expense => <ExpenseItem 
-                    key={ expense.id }
-                    title={ expense.title } 
-                    amount={ expense.amount } 
-                    date={ expense.date } 
-                /> ) } 
-            
+                {/* { filteredList.length === 0 
+                    ? 
+                        ( <p>No expenses found.</p> ) 
+                    : 
+                        ( filteredList.map( expense => 
+                        <ExpenseItem 
+                            key={ expense.id }
+                            title={ expense.title } 
+                            amount={ expense.amount } 
+                            date={ expense.date } 
+                        /> ) )
+                } */}
+                {/* could also use:
+
+                filteredList.length === 0 && <p>No expenses found.</p>
+                filteredList.length > 0 && 
+                    filteredList.map( expense => 
+                        <ExpenseItem 
+                            key={ expense.id }
+                            title={ expense.title } 
+                            amount={ expense.amount } 
+                            date={ expense.date } 
+                        /> ) 
+
+                ...instead of the ternary operator used above
+                
+                OR
+                
+                for the if statement we did above we would just do:
+
+                        
+                
+                
+                */}
+            { expensesContent }
             </Card>
         </div>
     )
