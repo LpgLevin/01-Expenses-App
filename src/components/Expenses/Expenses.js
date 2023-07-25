8c4020549041cfefaca2225b801529f0bc45b828
LpgLevin <lpglevin@gmail.com>
2023-07-25 15:35:02 +0100

    refactor adding ExpenseList file

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,48 +1,17 @@
 import React, { useState, useEffect } from "react";
 import "./Expenses.css";
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "../Filter/ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 function Expenses( { items } ) {
 
     const [ filteredYear, setFilteredYear ] = useState( "2020" );
-    // const [ filteredList, setFilteredList ] = useState( items );
 
     function filterChangeHandler( selectedYear ) {
 
         setFilteredYear( selectedYear );  
 
-        // if ( selectedYear === "2019" ) {
-            //      filteredExpenses = items.filter( function( expense ) {
-            //     return expense.date.getFullYear() === 2019;
-            //   } );
-             
-        // }; 
-
-
-        // if ( selectedYear === "2020" ) {
-        //          filteredExpenses = items.filter( function( expense ) {
-        //         return expense.date.getFullYear() === 2020;
-        //       } );
-        // };
-
-
-        // if ( selectedYear === "2021" ) {
-        //          filteredExpenses = items.filter( function( expense ) {
-        //         return expense.date.getFullYear() === 2021;
-        //       } );
-        // };
-
-         
-        // if ( selectedYear === "2022" ) {
-        //          filteredExpenses = items.filter( function( expense ) {
-        //         return expense.date.getFullYear() === 2022;
-        //       } );
-              
-        // };        
-        
-        // setFilteredList( filteredExpenses );
     };
 
         const filteredList = items.filter( function( expense ) {
@@ -50,21 +19,7 @@ function Expenses( { items } ) {
         });
 
                 
-        let expensesContent = <p>No expenses found.</p>;
-
-        if ( filteredList.length > 0 ) {
-
-        expensesContent = filteredList.map( ( expense )=> (
-                        
-            <ExpenseItem 
-                key={ expense.id }
-                title={ expense.title } 
-                amount={ expense.amount } 
-                date={ expense.date } 
-            /> 
-          ) ); 
-                        
-        };
+        
 
     return (
 
@@ -74,42 +29,8 @@ function Expenses( { items } ) {
                     selected={ filteredYear }
                     onChangeFilter={ filterChangeHandler } 
                 />    
-                
-                {/* { filteredList.length === 0 
-                    ? 
-                        ( <p>No expenses found.</p> ) 
-                    : 
-                        ( filteredList.map( expense => 
-                        <ExpenseItem 
-                            key={ expense.id }
-                            title={ expense.title } 
-                            amount={ expense.amount } 
-                            date={ expense.date } 
-                        /> ) )
-                } */}
-                {/* could also use:
-
-                filteredList.length === 0 && <p>No expenses found.</p>
-                filteredList.length > 0 && 
-                    filteredList.map( expense => 
-                        <ExpenseItem 
-                            key={ expense.id }
-                            title={ expense.title } 
-                            amount={ expense.amount } 
-                            date={ expense.date } 
-                        /> ) 
-
-                ...instead of the ternary operator used above
-                
-                OR
-                
-                for the if statement we did above we would just do:
-
-                        
-                
-                
-                */}
-            { expensesContent }
+                <ExpensesList filteredList={ filteredList }/>
+               
             </Card>
         </div>
     )
---
+++ src/components/Expenses/ExpensesList.js
@@ -0,0 +1,32 @@
+import ExpenseItem from "./ExpenseItem";
+
+function ExpensesList({ filteredList }) {
+
+        if ( filteredList.length === 0 ) {
+
+        return (
+            <h2 className="expenses-list__fallback" >
+                Found no expenses.
+            </h2>
+        )
+                        
+        };
+
+        return(
+
+            <ul className="expenses-list" >
+                {filteredList.map( ( expense )=> (
+                        
+                        <ExpenseItem 
+                            key={ expense.id }
+                            title={ expense.title } 
+                            amount={ expense.amount } 
+                            date={ expense.date } 
+                        /> 
+                      ))}
+            </ul>
+        )
+
+};
+
+export default ExpensesList;
